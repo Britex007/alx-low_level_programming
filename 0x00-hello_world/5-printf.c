c8225d7362964bec379058a0398164deef8ca576
Bright <firstbrite@gmail.com>
2023-02-16 19:08:04 -0800

    A program that prints a line with the printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - A program that priints a line  using the printf function
+ * Return: Always 0 (indicates success)
+ */
+int main(void)
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
