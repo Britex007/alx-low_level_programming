f5572275beb512380c5f781b89b5919dae4793d9
Bright <firstbrite@gmail.com>
2023-02-16 18:24:01 -0800

    printing with puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Program prints a line with the puts function
+ * Returns: Always 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
